513ac3c8062e699bd5ee2cdc6bb6d5d0cd3e941c
Miguel Ángel <miguel.calero@imagemaker.com>
2022-12-19 16:36:28 +0100

    feat(config): custom config

--- keyboards/lily58/glow_enc/config.h
+++ keyboards/lily58/glow_enc/config.h
@@ -21,9 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* key matrix size */
-// Rows are doubled-up
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 6
+#define DIODE_DIRECTION COL2ROW
 
 // wiring of each half
 #define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
@@ -34,15 +34,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SOFT_SERIAL_PIN D2
 
-#define RGB_DI_PIN F4
-
-#define RGBLED_NUM 72    // Number of LEDs
-
+/* ws2812 RGB LED */
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 72
 #define RGBLED_SPLIT { 36, 36 }
 #define RGBLIGHT_SPLIT
 #define RGBLIGHT_LIMIT_VAL 120
+#define WS2812_TRST_US 80
+#define RGBLIGHT_ANIMATIONS
 
-#define ENCODERS_PAD_A { F5 }
-#define ENCODERS_PAD_B { D4 }
-#define ENCODERS_PAD_A_RIGHT { D4 }
-#define ENCODERS_PAD_B_RIGHT { F5 }
+/* Encoders */
+#define ENCODERS_PAD_A { D4 }
+#define ENCODERS_PAD_B { F5 }
+#define ENCODERS_PAD_A_RIGHT { F5 }
+#define ENCODERS_PAD_B_RIGHT { D4 }
+#define ENCODER_RESOLUTION 4
+#define TAP_CODE_DELAY 10
--- keyboards/lily58/glow_enc/rules.mk
+++ keyboards/lily58/glow_enc/rules.mk
@@ -1,8 +1,12 @@
-ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+ENCODER_ENABLE = no       # ENables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
-LTO_ENABLE = yes 		   # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
+RGBLIGHT_DRIVER = WS2812
+LTO_ENABLE = no 		   # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
 BOOTMAGIC_ENABLE = no
 RGB_MATRIX_ENABLE = no     # Per key rgb leds
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+SPLIT_KEYBOARD = yes
+OLED_ENABLE= yes
+OLED_DRIVER = SSD1306
