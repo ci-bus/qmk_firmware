1e8b45a486d15556c011b804753cbec80ff4403b
Miguel Calero <miguel.calero@bimaxpro.net>
2026-05-02 13:15:52 +0200

    Hack Navi

---
+++ keyboards/miguelio/hack_navi/config.h
@@ -0,0 +1,14 @@
+// Copyright 2018-2022 Nick Brassel (@tzarc)
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+// Display Configuration
+#define SPI_SCK_PIN B3
+#define SPI_MOSI_PIN B5
+#define SPI_MISO_PIN B4
+
+#define TFT_CS_PIN B9
+#define TFT_DC_PIN B8
+#define TFT_RST_PIN B7
+
+#define QUANTUM_PAINTER_SUPPORTS_NATIVE_COLORS TRUE
---
+++ keyboards/miguelio/hack_navi/readme.md
@@ -0,0 +1,13 @@
+# magnetic viking
+
+Make example for this keyboard (after setting up your build environment):
+
+    make miguelio/hack_navi:default
+
+Flashing example for this keyboard:
+
+    make miguelio/hack_navi:default:flash
+
+# QMK
+QMK Firmware 0.28.0
+version 1.1.2
